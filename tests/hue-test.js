388a21b53da7bd7f99e6c7089d7ba57b54465715
Bryan Johnson <bjohnso5@gmail.com>
2013-01-19 01:26:33 -0500

    Multiple updates
    Added minified versions of hue.js and colors.js, converted tabs to
    spaces in all files (excluding html), added setIpAndApiKey function to
    hue object for pre-usage configuration (avoiding having to modify the
    javascript source to start using the library). Modified test files to
    use the minified versions for sanity checks.

--- tests/hue-test.js
+++ tests/hue-test.js
@@ -1,15 +1,32 @@
 var hue = hue || window.hue,
-	onState = { on: true },
-	offState = { on: false };
+    onState = { on: true },
+    offState = { on: false },
+    fullBrightness = { bri: 254 };
 
-module( "turnOnAll" );
-test("test return data", function() {
-	var data = hue.turnOnAll();
-	deepEqual(data, onState);
-});
+// set Hue bridge IP address and API key to use for test suite
+hue.setIpAndApiKey('192.168.11.10', '9cf5fd0c39b5845d0b8099e9c0daf2cd');
 
 module( "turnOffAll" );
 test("test return data", function() {
-	var data = hue.turnOffAll();
-	deepEqual(data, offState);
+    var data = hue.turnOffAll();
+    deepEqual(data, offState);
+});
+
+module( "turnOnAll" );
+test("test return data", function() {
+    var data = hue.turnOnAll();
+    deepEqual(data, onState);
+});
+
+module( "dim" );
+test("test return data", function() {
+    var data = hue.dim(3);
+    deepEqual(data, { bri: 244 });
+});
+
+module( "raise" );
+test("test return data", function() {
+    hue.setBrightness(3, 254);
+    var data = hue.raise(3);
+    deepEqual(data, fullBrightness);
 });
