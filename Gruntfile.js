f59eac653139a6dca4c46dce6c3af94803c32fa6
Bryan Johnson <bjohnso5@gmail.com>
2013-07-19 19:29:06 -0400

    Added additional setup tasks
    Updated grunt tasks to include a way to set the API Key, and to discover
    a connected Hue bridge via UPnP

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,9 +39,22 @@ module.exports = function(grunt) {
 		watch: {
 			files: ['<%= jshint.files %>'],
 			tasks: ['jshint', 'qunit']
+		},
+		preprocess: {
+			js: {
+				src: 'test/hue-test.preprocessed.js',
+				dest: 'test/hue-test.js',
+				options: {
+					context: {
+						IPAddress: grunt.file.readJSON('.discovered-ip').internalipaddress,
+						APIKey: grunt.file.isFile('.api-key') ? grunt.file.readJSON('.api-key').apiKey : ""
+					}
+				}
+			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-preprocess');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
@@ -53,4 +66,26 @@ module.exports = function(grunt) {
 	
 	// test, concat and uglify for distribution
 	grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+	grunt.registerTask('getBridgeIP', 'Get connected Hue bridge IP via upnp', function() {
+		this.async();
+		var needle = require('needle');
+		needle.get('http://www.meethue.com/api/nupnp', function(error, response, body) {
+			var responseJson = JSON.parse(body);
+			grunt.log.writeln("Found IP: " + responseJson[0].internalipaddress);
+			grunt.file.write('.discovered-ip', JSON.stringify(responseJson[0]));
+		});
+	});
+
+	grunt.registerTask('setAPIKey', 'Set API key for test run', function(apiKey) {
+		grunt.log.writeln("Saved api key: " + apiKey);
+		grunt.file.write('.api-key', JSON.stringify({ "apiKey": apiKey }));
+	});
+
+	// init library
+	grunt.registerTask('init', 'Initialize library with api key for tests', function(apiKey) {
+		grunt.log.writeln('Initializing with api key: ' + apiKey);
+		grunt.config.set('config.apiKey', apiKey);
+		grunt.task.run(['setAPIKey:' + apiKey, 'getBridgeIP', 'preprocess']);
+	});
 };
